7e3a5f83f7718e562ae2b4b233e1893ef9d7a890
botvkk <vivekt077@gmail.com>
2020-08-26 10:47:39 +0530

    added sorting features

--- src/app/state-wise-details/state-wise-details.component.ts
+++ src/app/state-wise-details/state-wise-details.component.ts
@@ -11,6 +11,7 @@ import { Router } from '@angular/router';
 export class StateWiseDetailsComponent implements OnInit {
 
   stateData
+  orderby: string = 'TotalCases'
 
   constructor(private dataService: DataService, private router: Router) { }
 
@@ -22,15 +23,36 @@ export class StateWiseDetailsComponent implements OnInit {
 
   stateDistrictData() {
     this.dataService.getData().subscribe(response => {
-
-      console.log(response)
       this.stateData = response
+      if (this.orderby == 'name') {
+        this.stateData.sort(function compare(a, b) {
+          if (a.state <= b.state) {
+            return -1;
+          }
+          else {
+            return 1;
+          }
+          // a must be equal to b
+          return 0;
+        });
+
+      }
+      else {
+        this.stateData.sort(function (a, b) {
+
+          return parseInt(b.confirmed) - parseInt(a.confirmed);
+        })
+      }
+
+
     })
   }
   getState(state) {
     this.router.navigate(['/state', state])
   }
-  onSelect(event) {
-    console.log(event)
+  onSelect(orderType) {
+    this.orderby = orderType
+    this.stateDistrictData()
   }
+
 }
